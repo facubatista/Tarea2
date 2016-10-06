f61ffe94708429b9ce27f1e04599b2b23155d5b9
RodCor19 <rodrigo.l4.his@gmail.com>
2016-10-06 03:11:06 -0300

    pull

--- web/JS/jreserva.js
+++ web/JS/jreserva.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     jQuery("#email").blur(function () {
         var mail = jQuery("#email").val();
         var request = new XMLHttpRequest();
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
                     document.getElementById("errEmail").style.display = 'block';
                     document.getElementById("botonRegistrar").disabled = true;
                 } else {
-                    document.getElementById("errEmail").style.display = 'none';ĺl
+                    document.getElementById("errEmail").style.display = 'none';
                     document.getElementById("botonRegistrar").disabled = false;
                 }
             }
@@ -59,4 +59,122 @@ jQuery(document).ready(function() {
             document.getElementById("botonRegistrar").style.display = 'initial';
         }
     });
+    jQuery("#dia").blur(function () {
+        var dia = jQuery("#dia").val();
+        if (dia === '') {
+            jQuery("#dia").val('');
+        }
+        if (isNaN(parseInt(dia))) {
+            jQuery("#dia").val('');
+        } else {
+            var numero = parseInt(dia);
+            if (numero >= 32) {
+                document.getElementById("dia").value = "31";
+            }
+        }
+    });
+    jQuery("#dia").blur(function () {
+        var dia = jQuery("#dia").val();
+        var mes = jQuery("#mes").val();
+        var anio = jQuery("#anio").val();
+        if (isNaN(parseInt(dia))) {
+            jQuery("#dia").val('');
+        } else {
+            var numero = parseInt(dia);
+            if (numero >= 30) {
+                if (numero === 31 && (mes === "Abril" || mes === "Junio" || mes === "Setiembre" || mes === "Noviembre")) {
+                    document.getElementById("dia").value = "30";
+                } else {
+                    if (numero >= 29 && (mes === "Febrero")) {
+                        if (parseInt(anio) && anio !== '') {
+                            var valor = parseInt(anio);
+                            if (numero >= 30 && mes === "Febrero")
+                                document.getElementById("dia").value = "29";
+                            if (valor % 4 !== 0) {
+                                document.getElementById("dia").value = "28";
+                            } else {
+                                if ((valor % 100 === 0)) {
+                                    if ((valor % 400 !== 0)) {
+                                        document.getElementById("dia").value = "28";
+                                    }
+                                }
+                            }
+                        } else {
+                            document.getElementById("dia").value = "29";
+                        }
+                    } else {
+                        document.getElementById("dia").value = "31";
+                    }
+
+                }
+            }
+        }
+    });
+    jQuery("#mes").click(function () {
+        var dia = jQuery("#dia").val();
+        var mes = jQuery("#mes").val();
+        var anio = jQuery("#anio").val();
+        if (dia !== '') {
+            var numero = parseInt(dia);
+            if (numero >= 30) {
+                if (numero === 31 && (mes === "Abril" || mes === "Junio" || mes === "Setiembre" || mes === "Noviembre")) {
+                    document.getElementById("dia").value = "30";
+                }
+                if (numero >= 30 && (mes === "Febrero")) {
+                    if (parseInt(anio) && anio !== '') {
+                        var valor = parseInt(anio);
+                        document.getElementById("dia").value = "29";
+                        if (valor % 4 !== 0) {
+                            document.getElementById("dia").value = "28";
+                        } else {
+                            if ((valor % 100 === 0)) {
+                                if ((valor % 400 !== 0)) {
+                                    document.getElementById("dia").value = "28";
+                                }
+                            }
+                        }
+                    } else {
+                        document.getElementById("dia").value = "29";
+                    }
+                }
+            }
+        }
+    });
+    jQuery("#anio").blur(function () {
+        var dia = jQuery("#dia").val();
+        var mes = jQuery("#mes").val();
+        var anio = jQuery("#anio").val();
+        if (anio !== '' && dia !== '') {
+            if (parseInt(anio)) {
+                var numero = parseInt(dia);
+                var valor = parseInt(anio);
+                if (numero >= 30 && mes === "Febrero")
+                    document.getElementById("dia").value = "29";
+                if (numero === 29 && mes === "Febrero" && (valor % 4 !== 0)) {
+                    document.getElementById("dia").value = "28";
+                } else {
+                    if ((valor % 100 === 0)) {
+                        if ((valor % 400 !== 0)) {
+                            document.getElementById("dia").value = "28";
+                        }
+                    }
+                }
+            }
+        }
+        
+            
+        var x = document.getElementById("botonRegistrar");
+        x.addEventListener("click", arreglos);
+//arreglar
+        function arreglos() {
+            var contrasena = document.getElementById("contraseña").value;
+            contrasena.value = sha1(contrasena);
+//            var imgVal = document.getElementById("imagen").value;
+//            if (imgVal !== ""){
+//                alert(imgVal);
+//            }
+            
+        }
+    });
 });
+       
